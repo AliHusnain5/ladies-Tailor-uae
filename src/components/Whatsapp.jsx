658c914c151f326ee641fd351bbcb00fbcfacb0a
AliHusnain5 <ah4008189@gmail.com>
2025-03-23 20:17:38 +0500

    new this

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -19,10 +19,43 @@ const WhatsApp = () => {
           height: '40px',
           border: 'none',
           cursor: 'pointer',
+          width: '100%',
+          maxWidth: '300px',
         }}
       >
         WhatsApp
       </button>
+
+      <style>
+        {`
+          @media (max-width: 600px) {
+            button {
+              padding-left: 30px;
+              background-size: 20px 20px;
+              height: 35px;
+              font-size: 0.9rem;
+            }
+          }
+
+          @media (min-width: 601px) and (max-width: 1024px) {
+            button {
+              padding-left: 35px;
+              background-size: 25px 25px;
+              height: 40px;
+              font-size: 1rem;
+            }
+          }
+
+          @media (min-width: 1025px) {
+            button {
+              padding-left: 40px;
+              background-size: 30px 30px;
+              height: 45px;
+              font-size: 1.1rem;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
